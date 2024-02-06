90830d5a2a1bbe1877509b415c2420e6fde91717
woojuice1102 <122527068+KangWooJu@users.noreply.github.com>
2024-02-06 20:11:08 +0900

    [Bronze V] Title: A/B, Time: 132 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        System.out.print(a/b);
+       
+    }
+}
