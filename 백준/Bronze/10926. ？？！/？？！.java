f3f7f63e79e7e836715f656a1fc6614fc9fa9964
woojuice1102 <122527068+KangWooJu@users.noreply.github.com>
2024-02-06 20:25:32 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        System.out.printf("%s??!",a);
+    }
+}
